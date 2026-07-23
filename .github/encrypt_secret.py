97e3d5d50d6afff99b5358fb7af114207cb1580d
jing he <jing.he.320@gmail.com>
2026-07-23 13:47:51 +0800

    Fix: remove extra base64 padding causing 32-byte key error

--- .github/encrypt_secret.py
+++ .github/encrypt_secret.py
@@ -1,5 +1,5 @@
 import base64, os, nacl.public
-pubkey = base64.b64decode(os.environ['GH_PUBKEY'] + '==')
+pubkey = base64.b64decode(os.environ['GH_PUBKEY'])
 token = os.environ['NEW_TOKEN'].encode()
 pk = nacl.public.PublicKey(pubkey)
 print(base64.b64encode(nacl.public.SealedBox(pk).encrypt(token)).decode())
